d87a27bce448f15fcbbec15cd02d4a89df23561b
AnoopGutti7 <gutiianoop1@gmail.com>
2026-04-27 19:35:15 +0530

    fix api url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://collab-editors-1.onrender.com/", {
+export const socket = io("https://colabeditor-1.onrender.com", {
   transports: ["polling", "websocket"],
 });
