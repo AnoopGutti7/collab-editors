5907b8c81d35fdc3d55e531cec033994297bad90
AnoopGutti7 <gutiianoop1@gmail.com>
2026-04-27 17:08:49 +0530

    cors policy fix

--- server/index.js
+++ server/index.js
@@ -7,19 +7,34 @@ import fs from "fs/promises";
 import path from "path";
 import { spawn } from "child_process";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+
+dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.use(cors({ origin: "*" }));
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is running...");
+});
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
   },
 });
 
